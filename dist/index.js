bedbd126c8f9228606c7077d8ce810030e653365
Sachith <sachithsujeewa@gmail.com>
2024-01-14 15:03:47 +0530

    add crud with mongo

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,49 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv = __importStar(require("dotenv"));
 const express_1 = __importDefault(require("express"));
+const database_1 = require("./database");
+const employee_routes_1 = require("./employee.routes");
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
-app.get('/', (_req, res) => {
-    return res.send('Express Typescript on Vercel');
-});
-app.get('/ping', (_req, res) => {
-    return res.send('pong 🏓');
-});
-app.listen(port, () => {
-    return console.log(`Server is listening on ${port}`);
+dotenv.config();
+const { ATLAS_URI } = process.env;
+(0, database_1.connectToDatabase)(ATLAS_URI || "mongodb://localhost:27017").then(() => {
+    app.use("/employees", employee_routes_1.employeeRouter);
+    app.get('/', (_req, res) => {
+        return res.send('Express Typescript on Vercel');
+    });
+    app.get('/ping', (_req, res) => {
+        return res.send('pong 🏓');
+    });
+    app.listen(port, () => {
+        return console.log(`Server is listening on ${port}`);
+    });
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
+import * as dotenv from "dotenv";
+import cors from "cors";
 import express, { Request, Response } from 'express'
+import { connectToDatabase } from "./database";
+import { employeeRouter } from "./employee.routes";
+
 
 const app = express()
 const port = process.env.PORT || 8080
 
-app.get('/', (_req: Request, res: Response) => {
-  return res.send('Express Typescript on Vercel')
-})
+dotenv.config();
 
-app.get('/ping', (_req: Request, res: Response) => {
-  return res.send('pong 🏓')
-})
+const { ATLAS_URI } = process.env;
 
-app.listen(port, () => {
-  return console.log(`Server is listening on ${port}`)
+connectToDatabase(ATLAS_URI || "mongodb://localhost:27017").then(() => {
+
+    app.use("/employees", employeeRouter);
+    app.get('/', (_req: Request, res: Response) => {
+        return res.send('Express Typescript on Vercel')
+    })
+
+    app.get('/ping', (_req: Request, res: Response) => {
+        return res.send('pong 🏓')
+    })
+    app.listen(port, () => {
+        return console.log(`Server is listening on ${port}`)
+    })
 })
